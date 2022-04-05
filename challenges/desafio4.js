0da64224dc7ef7c3a5ca4cc7861af4a010fd2da0
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-05 15:44:52 -0300

    Requisito 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,12 @@
+db.produtos
+  .find(
+    {
+      $and: [
+        { vendidos: { $gt: 50, $lt: 100 } },
+      ],
+    },
+    { _id: 0, nome: 1, vendidos: 1 },
+  )
+  .sort(
+    { vendidos: 1 },
+  );
