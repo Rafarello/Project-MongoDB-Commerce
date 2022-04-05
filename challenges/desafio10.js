ae0dd69845e903c14c2716c5336984cd31aa9a76
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-05 18:10:57 -0300

    Refatorando requisito 10 - Corrigindo campo filtro para percentual (antes estava quantidade)

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,7 +3,7 @@ db.produtos.find(
     valoresNutricionais: {
       $elemMatch: {
         tipo: "proteínas",
-        quantidade: { $gte: 30, $lte: 40 },
+        percentual: { $gte: 30, $lte: 40 },
       },
     },
   },
