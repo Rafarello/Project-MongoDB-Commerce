5d83fc0b5162ba450e3626b5d35eda58f8e2b661
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-05 17:08:22 -0300

    Requisito 9

---
+++ challenges/desafio9.js
@@ -0,0 +1,11 @@
+db.produtos.find(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: "calorias",
+        quantidade: { $lt: 500 },
+      },
+    },
+  },
+  { _id: 0, nome: 1 },
+);
