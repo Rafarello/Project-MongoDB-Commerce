55c37c93ac14619060ce204ee1e5e1775d2f2ac7
Rafael Moraes <rafael1moraes1rocha@gmail.com>
2022-04-05 16:12:30 -0300

    Requisito 7

---
+++ challenges/desafio7.js
@@ -0,0 +1,10 @@
+db.produtos
+  .find(
+    {
+      $and: [
+        { vendidos: { $not: { $eq: 50 } } },
+        { tags: { $exists: false } },
+      ],
+    },
+    { _id: 0, nome: 1, vendidos: 1 },
+  );
